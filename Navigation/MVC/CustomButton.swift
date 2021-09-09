2a209c7e1d0900f1f398b5dd4aa74fe4829563b8
AlexeyKharin <9goofy6@gmail.com>
2021-09-09 22:24:53 +0300

    Completed Task-6

---
+++ Navigation/MVC/CustomButton.swift
@@ -0,0 +1,32 @@
+
+import Foundation
+import UIKit
+
+final class CustomButton: UIButton {
+    
+    var onTap: (() -> Void)?
+    
+    init(tittle: String, cornerRadius: CGFloat, backgroundColor: UIColor, titTileColor: UIColor, onTap: (() -> Void)?) {
+        self.onTap = onTap
+        
+        super.init(frame: .zero)
+        
+        self.setTitle(tittle, for: .normal)
+        self.layer.cornerRadius = cornerRadius
+        
+        self.setTitleColor(titTileColor, for: .normal)
+        
+        self.backgroundColor = backgroundColor
+        toAutoLayout()
+        addTarget(self, action: #selector(tapped), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func tapped() {
+        onTap?()
+        
+    }
+}
---
+++ Navigation/MVC/FeedViewController.swift
@@ -0,0 +1,78 @@
+import UIKit
+final class FeedViewController: UIViewController {
+    
+    var model: ModelCheck
+    
+    init(model: ModelCheck) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var customButton: CustomButton = {
+        let loginButton = CustomButton(tittle: "Check", cornerRadius: 5, backgroundColor: .yellow, titTileColor: .green) { [weak self] in
+            guard let intermediateSafety = self?.intermediate else { return }
+            self?.model.check(password: intermediateSafety)
+        }
+        return loginButton
+    }()
+    
+    var intermediate: String?
+        
+    lazy var resultOfCheck: UIView = {
+        let resultOfCheck = UIView()
+        resultOfCheck.toAutoLayout()
+        resultOfCheck.backgroundColor = .white
+        return resultOfCheck
+    }()
+    
+    lazy var customTextField: CustomTextField = {
+        
+        let textField = CustomTextField(placeholder: "CheckPassword") { [weak self] text in
+            self?.intermediate = text
+        }
+        
+        textField.backgroundColor = .white
+        textField.layer.cornerRadius = 5
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        view.addSubview(customButton)
+        view.addSubview(customTextField)
+        view.addSubview(resultOfCheck)
+        
+        model.response = {[weak self] bool in
+            switch bool {
+            case true:
+                self?.resultOfCheck.backgroundColor = .green
+            case false:
+                self?.resultOfCheck.backgroundColor = .red
+            }
+        }
+        
+        let constraints = [
+            customButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            customButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            customButton.widthAnchor.constraint(equalToConstant: 200),
+            customButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            customTextField.centerXAnchor.constraint(equalTo: customButton.centerXAnchor),
+            customTextField.widthAnchor.constraint(equalToConstant: 200),
+            customTextField.heightAnchor.constraint(equalToConstant: 40),
+            customTextField.bottomAnchor.constraint(equalTo: customButton.topAnchor, constant: -20),
+            
+            resultOfCheck.centerXAnchor.constraint(equalTo: customButton.centerXAnchor),
+            resultOfCheck.widthAnchor.constraint(equalToConstant: 200),
+            resultOfCheck.heightAnchor.constraint(equalToConstant: 100),
+            resultOfCheck.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
